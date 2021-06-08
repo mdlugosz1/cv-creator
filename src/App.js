c3fbe2f11e7acfa0f0abcbec6c38291c4118e4aa
Michał Długosz <mdugosz@gmail.com>
2021-06-08 17:46:29 +0200

    Add basic structure

--- src/App.js
+++ src/App.js
@@ -1,1 +1,43 @@
+import React from 'react';
+import './App.css';
+import PersonalInput from './components/PersonalInput';
+import Personal from './components/PersonalPreview';
 
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { personal: { name: '', lastname: '', webpage: '' } };
+    this.handlePersonalChange = this.handlePersonalChange.bind(this);
+  }
+
+  handlePersonalChange(info) {
+    this.setState({
+      personal: Object.assign({}, this.state.personal, info),
+    });
+    console.log(this.state);
+  }
+
+  render() {
+    const { name, lastname, phone, city, mail, position } = this.state.personal;
+    return (
+      <div>
+        <section className="inputs">
+          <PersonalInput onInputChange={this.handlePersonalChange} />
+        </section>
+        <div className="cv">
+          <section className="left-panel">
+            <Personal
+              name={name}
+              lastname={lastname}
+              phone={phone}
+              city={city}
+              mail={mail}
+              position={position}
+            />
+          </section>
+          <section className="right-panel"></section>
+        </div>
+      </div>
+    );
+  }
+}
