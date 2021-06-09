d593f56a9b3e42c0b153d9a7a4eab1637b998911
Michał Długosz <mdugosz@gmail.com>
2021-06-09 20:21:02 +0200

    Add preview for personal details

--- src/components/PersonalPreview.js
+++ src/components/PersonalPreview.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import Headers from './Utilities/Headers';
 
 const Personal = (props) => {
-  const { name, lastname, phone, city, mail, position } = props;
+  const { name, lastname, phone, city, mail, position, webpage } = props;
   return (
     <div>
       <h1>{name + ' ' + lastname}</h1>
+      <h3>{position}</h3>
       <div>
-        {phone !== undefined && (
-          <div>
-            <h1>Contact</h1>
-            <h3>
-              <i className="fas fa-phone" />
-              {phone}
-            </h3>
-          </div>
-        )}
+        <div>
+          <Headers title={'Contact'} />
+          <p>
+            <i className="fas fa-phone" />
+            {phone}
+          </p>
+          <p>
+            <i className="fas fa-envelope" />
+            {mail}
+          </p>
+          <p>
+            <i className="fas fa-map-marker-alt" />
+            {city}
+          </p>
+          <p>
+            <i className="fab fa-github" />
+            {webpage}
+          </p>
+        </div>
       </div>
     </div>
   );
