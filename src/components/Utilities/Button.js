352e8a2f85c3df83ba759d05ec64796c1d03b925
Michał Długosz <mdugosz@gmail.com>
2021-06-22 18:48:40 +0200

    Add data-id property for buttons

--- src/components/Utilities/Button.js
+++ src/components/Utilities/Button.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Button = (props) => {
-  return <button onClick={props.click}>{props.name}</button>;
+  return (
+    <button onClick={props.click} data-id={props.data}>
+      {props.name}
+    </button>
+  );
 };
 
 export default Button;
