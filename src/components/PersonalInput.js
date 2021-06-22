772a92b375de933fb15eef34b5affdadad6f83ca
Michał Długosz <mdugosz@gmail.com>
2021-06-22 18:47:31 +0200

    Refactor to use Form component

--- src/components/PersonalInput.js
+++ src/components/PersonalInput.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import Form from './Utilities/Form';
+import uniqid from 'uniqid';
 
 export default class PersonalInput extends React.Component {
   constructor(props) {
     super(props);
-    this.whenChange = this.whenChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      inputs: [
+        { type: 'text', name: 'name', placeholder: 'First Name', id: uniqid() },
+        {
+          type: 'text',
+          name: 'lastname',
+          placeholder: 'Last Name',
+          id: uniqid(),
+        },
+        { type: 'text', name: 'position', placeholder: 'Title', id: uniqid() },
+        { type: 'text', name: 'mail', placeholder: 'E-mail', id: uniqid() },
+        {
+          type: 'text',
+          name: 'phone',
+          placeholder: 'Phone Number',
+          id: uniqid(),
+        },
+        {
+          type: 'text',
+          name: 'webpage',
+          placeholder: 'Webpage URL',
+          id: uniqid(),
+        },
+        { type: 'text', name: 'city', placeholder: 'City', id: uniqid() },
+      ],
+    };
   }
 
-  whenChange(e) {
+  handleChange(e) {
     const target = e.target;
     const value = target.value;
     const key = target.name;
@@ -15,9 +43,11 @@ export default class PersonalInput extends React.Component {
   }
 
   render() {
+    const { inputs } = this.state;
     return (
       <div>
-        <form>
+        <Form inputs={inputs} handleChange={this.handleChange} />
+        {/* <form>
           <input
             type="text"
             name="name"
@@ -60,7 +90,7 @@ export default class PersonalInput extends React.Component {
             placeholder="City"
             onChange={this.whenChange}
           />
-        </form>
+        </form> */}
       </div>
     );
   }
