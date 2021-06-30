f3e204deb460e6f8b91d3c7d0fc0a7c4ed7c9de5
Michał Długosz <mdugosz@gmail.com>
2021-06-30 22:30:24 +0200

    Add basic ProjectsPreview component

---
+++ src/components/ProjectsPreview.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Headers from './Utilities/Headers';
+
+const Projects = (props) => {
+  return (
+    <div>
+      <Headers title="Projects" />
+      <h2>Project title</h2>
+      <div>
+        <h3>Demo</h3>
+        <h3>Repository</h3>
+      </div>
+      <p>
+        To jest opis projektu. To jest opis projektu.To jest opis projektu.To
+        jest opis projektu.To jest opis projektu.To jest opis projektu.To jest
+        opis projektu.To jest opis projektu.To jest opis projektu.
+      </p>
+    </div>
+  );
+};
+
+export default Projects;
