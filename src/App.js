31a4b043f1c01cee9c3d6d92163b4d144fe5a178
Michał Długosz <mdugosz@gmail.com>
2021-07-01 22:36:14 +0200

    Refactor render method
    Now every main element of application is rendered via separate method to
    avoid code mess in App render method.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import SkillsInput from './components/SkillsInput';
 import Skills from './components/SkillsPreview';
 import LanguagesInput from './components/LanguagesInput';
 import Languages from './components/LanguagesPreview';
+import Projects from './components/ProjectsPreview';
+import ProjectsInput from './components/ProjectsInput';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,6 +16,7 @@ export default class App extends React.Component {
       personal: { name: '', lastname: '', webpage: '' },
       skills: [],
       languages: [],
+      projects: [],
     };
     this.handleChange = this.handleChange.bind(this);
     this.removeData = this.removeData.bind(this);
@@ -46,55 +49,74 @@ export default class App extends React.Component {
         [dataKey]: Object.assign(stateObject, userObject),
       });
     }
+
+    console.log(this.state);
   }
 
   removeData(itemIndex, key) {
     this.setState((prevState) => {
       const newState = prevState[key];
       newState.splice(itemIndex, 1);
-
       return { [key]: newState };
     });
   }
 
-  render() {
+  setInputs() {
+    return (
+      <section className="inputs">
+        <PersonalInput onInputChange={this.handleChange} />
+        <h1>Skills</h1>
+        <SkillsInput
+          onInputChange={this.handleChange}
+          removeData={this.removeData}
+        />
+        <h1>Languages</h1>
+        <LanguagesInput
+          onInputChange={this.handleChange}
+          removeData={this.removeData}
+        />
+        <h1>Projects</h1>
+        <ProjectsInput
+          onInputChange={this.handleChange}
+          removeData={this.removeData}
+        />
+      </section>
+    );
+  }
+
+  setPreview() {
     const { name, lastname, phone, city, mail, position, webpage } =
       this.state.personal;
 
-    const { skills } = this.state;
-    const { languages } = this.state;
+    const { skills, languages, projects } = this.state;
 
     return (
-      <div className="content">
-        <section className="inputs">
-          <PersonalInput onInputChange={this.handleChange} />
-          <h1>Skills</h1>
-          <SkillsInput
-            onInputChange={this.handleChange}
-            removeData={this.removeData}
+      <div className="cv">
+        <section className="left-panel">
+          <Personal
+            name={name}
+            lastname={lastname}
+            phone={phone}
+            city={city}
+            mail={mail}
+            position={position}
+            webpage={webpage}
           />
-          <h1>Languages</h1>
-          <LanguagesInput
-            onInputChange={this.handleChange}
-            removeData={this.removeData}
-          />
+          <Skills skill={skills} />
+          <Languages languages={languages} />
         </section>
-        <div className="cv">
-          <section className="left-panel">
-            <Personal
-              name={name}
-              lastname={lastname}
-              phone={phone}
-              city={city}
-              mail={mail}
-              position={position}
-              webpage={webpage}
-            />
-            <Skills skill={skills} />
-            <Languages languages={languages} />
-          </section>
-          <section className="right-panel"></section>
-        </div>
+        <section className="right-panel">
+          <Projects projects={projects} />
+        </section>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="content">
+        {this.setInputs()}
+        {this.setPreview()}
       </div>
     );
   }
