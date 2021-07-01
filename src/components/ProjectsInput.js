800845d86c728b998f70dbf06f860e7e1d107ad8
Michał Długosz <mdugosz@gmail.com>
2021-07-01 22:31:13 +0200

    Refactor component
    ProjectsInput now has new methods for creating new inputs and deleting
    existing ones. InputTemplate was removed from class contructor.

--- src/components/ProjectsInput.js
+++ src/components/ProjectsInput.js
@@ -6,101 +6,87 @@ export default class ProjectsInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      projects: [
-        {
-          mainID: uniqid(),
-          inputs: [
-            {
-              id: uniqid(),
-              type: 'text',
-              name: 'project-name',
-              placeholder: 'Project name',
-            },
-            {
-              id: uniqid(),
-              type: 'text',
-              name: 'demo',
-              placeholder: 'Live version link',
-            },
-            {
-              id: uniqid(),
-              type: 'text',
-              name: 'live',
-              placeholder: 'Repository link',
-            },
-            {
-              id: uniqid(),
-              name: 'descripiton',
-              tag: 'textarea',
-              placeholder: 'Project description',
-            },
-          ],
-        },
-        {
-          mainID: uniqid(),
-          inputs: [
-            {
-              id: uniqid(),
-              type: 'text',
-              name: 'project-name',
-              placeholder: 'Project name',
-            },
-            {
-              id: uniqid(),
-              type: 'text',
-              name: 'demo',
-              placeholder: 'Live version link',
-            },
-            {
-              id: uniqid(),
-              type: 'text',
-              name: 'live',
-              placeholder: 'Repository link',
-            },
-            {
-              id: uniqid(),
-              name: 'descripiton',
-              tag: 'textarea',
-              placeholder: 'Project description',
-            },
-          ],
-        },
-      ],
+      projects: [this.inputTemplate()],
       button: 'Add new project',
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.removeProject = this.removeProject.bind(this);
+    this.removeInputBlock = this.removeInputBlock.bind(this);
+    this.createNewInputs = this.createNewInputs.bind(this);
+  }
+
+  inputTemplate() {
+    return {
+      mainID: uniqid(),
+      inputs: [
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'name',
+          placeholder: 'Project name',
+        },
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'demo',
+          placeholder: 'Live version link',
+        },
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'repository',
+          placeholder: 'Repository link',
+        },
+        {
+          id: uniqid(),
+          name: 'descripiton',
+          tag: 'textarea',
+          placeholder: 'Project description',
+        },
+      ],
+    };
+  }
+
+  getMainStateKey() {
+    return Object.keys(this.state)
+      .filter((key) => key !== 'button')
+      .toString();
   }
 
   handleChange(e) {
     this.props.onInputChange({
-      projects: {
+      [this.getMainStateKey()]: {
         [e.target.name]: e.target.value,
         id: e.target.closest('div').dataset.mainId,
       },
     });
   }
 
-  removeProject(e) {
+  removeInputBlock(e) {
     const mainId = e.target.closest('div').dataset.mainId;
-    const index = this.state.projects.indexOf(
-      this.state.projects.find(({ mainID }) => mainID === mainId)
+    const key = this.getMainStateKey();
+
+    const index = this.state[key].indexOf(
+      this.state[key].find(({ mainID }) => mainID === mainId)
     );
 
-    const key = 'projects';
     this.setState((prevState) => {
-      const newProjects = prevState.projects;
-      newProjects.splice(index, 1);
-      return { projects: newProjects };
+      const newState = prevState[key];
+      newState.splice(index, 1);
+      return { [key]: newState };
     });
 
     this.props.removeData(mainId, key);
   }
 
-  /* createProjectInputs() {
-    this.setState({});
-  } */
+  createNewInputs() {
+    const key = this.getMainStateKey();
+    const newInputs = this.inputTemplate();
+    console.log(newInputs);
+    this.setState({
+      [key]: [...this.state[key], newInputs],
+    });
+  }
 
   render() {
     const { projects, button } = this.state;
@@ -109,7 +95,8 @@ export default class ProjectsInput extends React.Component {
         inputs={projects}
         button={button}
         handleChange={this.handleChange}
-        deleteInput={this.removeProject}
+        deleteInput={this.removeInputBlock}
+        createInput={this.createNewInputs}
       />
     );
   }
