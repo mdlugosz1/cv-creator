31cd9a7f42bf7d0b8a65997ff1b6d9939272ee3e
Michał Długosz <mdugosz@gmail.com>
2021-07-05 21:36:52 +0200

    Add EducationInput component

---
+++ src/components/EducationInput.js
@@ -0,0 +1,58 @@
+import ProjectsInput from './ProjectsInput';
+import uniqid from 'uniqid';
+import Form from './Utilities/Form';
+
+export default class EducationInput extends ProjectsInput {
+  constructor(props) {
+    super(props);
+    this.state = {
+      education: [this.inputTemplate()],
+      button: 'Add education',
+    };
+  }
+
+  inputTemplate() {
+    return {
+      mainID: uniqid(),
+      inputs: [
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'univeristy',
+          placeholder: 'University',
+        },
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'degree',
+          placeholder: 'Degree',
+        },
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'startDate',
+          placeholder: 'Start date MM/YYYY',
+        },
+        {
+          id: uniqid(),
+          name: 'endDate',
+          type: 'text',
+          placeholder: 'End date MM/YYYY',
+        },
+      ],
+    };
+  }
+
+  render() {
+    const { education, button } = this.state;
+    return (
+      <Form
+        inputs={education}
+        button={button}
+        handleChange={this.handleChange}
+        deleteInput={this.removeInputBlock}
+        createInput={this.createNewInputs}
+      />
+    );
+  }
+}
