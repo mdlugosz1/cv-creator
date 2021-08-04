1175bfa9b89d237a5b8038d3f81beeaaefffc0a2
Michał Długosz <mdugosz@gmail.com>
2021-08-04 22:48:45 +0200

    Add class name property

--- src/components/Utilities/Button.js
+++ src/components/Utilities/Button.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../../styles/Buttons.css';
 
 const Button = (props) => {
-  return (
-    <button onClick={props.click} data-id={props.data}>
-      {props.name}
-    </button>
-  );
+    return (
+        <button onClick={props.click} data-id={props.data} className={props.class}>
+            {props.name}
+        </button>
+    );
 };
 
 export default Button;
