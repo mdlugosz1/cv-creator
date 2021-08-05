b0f2f527b4fb85a6153e2c14cb3616b0f828b408
Michał Długosz <mdugosz@gmail.com>
2021-08-05 14:20:51 +0200

    Add styling

--- src/App.js
+++ src/App.js
@@ -14,129 +14,111 @@ import EducationInput from './components/EducationInput';
 import EducationPreview from './components/EducationPreview';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      personal: { name: '', lastname: '', webpage: '' },
-      skills: [],
-      languages: [],
-      projects: [],
-      work: [],
-      education: [],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.removeData = this.removeData.bind(this);
-  }
-
-  addUserInputToArray(key, currStateObj, userObject) {
-    let currentObject = currStateObj.find(({ id }) => id === userObject.id);
-
-    if (currentObject) {
-      this.setState((prevState) => {
-        Object.assign(currentObject, userObject);
-        return { [key]: prevState[key] };
-      });
-    } else {
-      this.setState({
-        [key]: [...currStateObj, Object.assign({}, userObject)],
-      });
-    }
-  }
-
-  handleChange(data) {
-    const dataKey = Object.keys(data);
-    const userObject = data[dataKey];
-    const stateObject = this.state[dataKey];
-
-    if (Array.isArray(stateObject)) {
-      this.addUserInputToArray(dataKey, stateObject, userObject);
-    } else {
-      this.setState({
-        [dataKey]: Object.assign(stateObject, userObject),
-      });
+    constructor(props) {
+        super(props);
+        this.state = {
+            personal: { name: '', lastname: '', webpage: '' },
+            skills: [],
+            languages: [],
+            projects: [],
+            work: [],
+            education: [],
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.removeData = this.removeData.bind(this);
     }
 
-    console.log(this.state);
-  }
+    addUserInputToArray(key, currStateObj, userObject) {
+        let currentObject = currStateObj.find(({ id }) => id === userObject.id);
 
-  removeData(itemIndex, key) {
-    this.setState((prevState) => {
-      const newState = prevState[key];
-      newState.splice(itemIndex, 1);
-      return { [key]: newState };
-    });
-  }
+        if (currentObject) {
+            this.setState((prevState) => {
+                Object.assign(currentObject, userObject);
+                return { [key]: prevState[key] };
+            });
+        } else {
+            this.setState({
+                [key]: [...currStateObj, Object.assign({}, userObject)],
+            });
+        }
+    }
 
-  setInputs() {
-    return (
-      <section className="inputs">
-        <h1>Personal Information</h1>
-        <PersonalInput onInputChange={this.handleChange} />
-        <h1>Skills</h1>
-        <SkillsInput
-          onInputChange={this.handleChange}
-          removeData={this.removeData}
-        />
-        <h1>Languages</h1>
-        <LanguagesInput
-          onInputChange={this.handleChange}
-          removeData={this.removeData}
-        />
-        <h1>Projects</h1>
-        <ProjectsInput
-          onInputChange={this.handleChange}
-          removeData={this.removeData}
-        />
-        <h1>Work experience</h1>
-        <WorkInput
-          onInputChange={this.handleChange}
-          removeData={this.removeData}
-        />
-        <h1>Education</h1>
-        <EducationInput
-          onInputChange={this.handleChange}
-          removeData={this.removeData}
-        />
-      </section>
-    );
-  }
+    handleChange(data) {
+        const dataKey = Object.keys(data);
+        const userObject = data[dataKey];
+        const stateObject = this.state[dataKey];
 
-  setPreview() {
-    const { name, lastname, phone, city, mail, position, webpage } =
-      this.state.personal;
+        if (Array.isArray(stateObject)) {
+            this.addUserInputToArray(dataKey, stateObject, userObject);
+        } else {
+            this.setState({
+                [dataKey]: Object.assign(stateObject, userObject),
+            });
+        }
+    }
 
-    const { skills, languages, projects, work, education } = this.state;
+    removeData(itemIndex, key) {
+        this.setState((prevState) => {
+            const newState = prevState[key];
+            newState.splice(itemIndex, 1);
+            return { [key]: newState };
+        });
+    }
 
-    return (
-      <div className="cv">
-        <section className="left-panel">
-          <Personal
-            name={name}
-            lastname={lastname}
-            phone={phone}
-            city={city}
-            mail={mail}
-            position={position}
-            webpage={webpage}
-          />
-          <Skills skill={skills} />
-          <Languages languages={languages} />
-        </section>
-        <section className="right-panel">
-          <Projects projects={projects} />
-          <WorkPreview experience={work} />
-          <EducationPreview education={education} />
-        </section>
-      </div>
-    );
-  }
+    setInputs() {
+        return (
+            <section className="inputs">
+                <h1>Personal Information</h1>
+                <PersonalInput onInputChange={this.handleChange} />
+                <h1>Skills</h1>
+                <SkillsInput onInputChange={this.handleChange} removeData={this.removeData} />
+                <h1>Languages</h1>
+                <LanguagesInput onInputChange={this.handleChange} removeData={this.removeData} />
+                <h1>Projects</h1>
+                <ProjectsInput onInputChange={this.handleChange} removeData={this.removeData} />
+                <h1>Work experience</h1>
+                <WorkInput onInputChange={this.handleChange} removeData={this.removeData} />
+                <h1>Education</h1>
+                <EducationInput onInputChange={this.handleChange} removeData={this.removeData} />
+            </section>
+        );
+    }
 
-  render() {
-    return (
-      <div className="content">
-        {this.setInputs()}
-        {this.setPreview()}
-      </div>
-    );
-  }
+    setPreview() {
+        const { name, lastname, phone, city, mail, position, webpage } = this.state.personal;
+
+        const { skills, languages, projects, work, education } = this.state;
+
+        return (
+            <div className="cv">
+                <section className="left-panel">
+                    <Personal
+                        name={name}
+                        lastname={lastname}
+                        phone={phone}
+                        city={city}
+                        mail={mail}
+                        position={position}
+                        webpage={webpage}
+                    />
+                    <Skills skill={skills} />
+                    <Languages languages={languages} />
+                </section>
+                <section className="right-panel">
+                    <Projects projects={projects} />
+                    <WorkPreview experience={work} />
+                    <EducationPreview education={education} />
+                </section>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="content">
+                {this.setInputs()}
+                {this.setPreview()}
+            </div>
+        );
+    }
 }
