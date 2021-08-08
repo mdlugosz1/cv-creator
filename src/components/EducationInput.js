ff46a29f7c831401c027ca27dcbd218eadf1c209
Michał Długosz <mdugosz@gmail.com>
2021-08-08 23:25:44 +0200

    Refactor into functional component, add hooks

--- src/components/EducationInput.js
+++ src/components/EducationInput.js
@@ -1,60 +1,105 @@
-import ProjectsInput from './ProjectsInput';
+import React, { useState } from 'react';
 import uniqid from 'uniqid';
 import Form from './Utilities/Form';
 
-export default class EducationInput extends ProjectsInput {
-    constructor(props) {
-        super(props);
-        this.state = {
-            education: [this.inputTemplate()],
-            button: 'Add',
-        };
-    }
+const EducationInput = (props) => {
+  const [educationInput, setEducationInput] = useState({
+    education: [
+      {
+        mainID: uniqid(),
+        inputs: [
+          {
+            id: uniqid(),
+            type: 'text',
+            name: 'university',
+            placeholder: 'University',
+          },
+          {
+            id: uniqid(),
+            type: 'text',
+            name: 'degree',
+            placeholder: 'Degree',
+          },
+          {
+            id: uniqid(),
+            type: 'text',
+            name: 'startDate',
+            placeholder: 'Start date MM/YYYY',
+          },
+          {
+            id: uniqid(),
+            name: 'endDate',
+            type: 'text',
+            placeholder: 'End date MM/YYYY',
+          },
+        ],
+      },
+    ],
+    button: 'Add',
+  });
 
-    inputTemplate() {
-        return {
-            mainID: uniqid(),
-            inputs: [
-                {
-                    id: uniqid(),
-                    type: 'text',
-                    name: 'university',
-                    placeholder: 'University',
-                },
-                {
-                    id: uniqid(),
-                    type: 'text',
-                    name: 'degree',
-                    placeholder: 'Degree',
-                },
-                {
-                    id: uniqid(),
-                    type: 'text',
-                    name: 'startDate',
-                    placeholder: 'Start date MM/YYYY',
-                },
-                {
-                    id: uniqid(),
-                    name: 'endDate',
-                    type: 'text',
-                    placeholder: 'End date MM/YYYY',
-                },
-            ],
-        };
-    }
+  const inputTemplate = () => {
+    return {
+      mainID: uniqid(),
+      inputs: [
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'university',
+          placeholder: 'University',
+        },
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'degree',
+          placeholder: 'Degree',
+        },
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'startDate',
+          placeholder: 'Start date MM/YYYY',
+        },
+        {
+          id: uniqid(),
+          name: 'endDate',
+          type: 'text',
+          placeholder: 'End date MM/YYYY',
+        },
+      ],
+    };
+  };
 
-    render() {
-        const { education, button } = this.state;
-        return (
-            <div>
-                <Form
-                    inputs={education}
-                    button={button}
-                    handleChange={this.handleChange}
-                    deleteInput={this.removeInputBlock}
-                    createInput={this.createNewInputs}
-                />
-            </div>
-        );
-    }
-}
+  const handleChange = (e) => {
+    const key = props.getMainKey(educationInput);
+    props.onInputChange({
+      [key]: {
+        [e.target.name]: e.target.value,
+        id: e.target.closest('div').dataset.mainId,
+      },
+    });
+  };
+
+  const removeInputBlock = (e) => {
+    props.removeInput(educationInput, props, setEducationInput, e);
+  };
+
+  const createNewInputs = () => {
+    const newInput = inputTemplate();
+    props.addInput(newInput, props, educationInput, setEducationInput);
+  };
+
+  return (
+    <div>
+      <Form
+        inputs={educationInput.education}
+        button={educationInput.button}
+        handleChange={handleChange}
+        deleteInput={removeInputBlock}
+        createInput={createNewInputs}
+      />
+    </div>
+  );
+};
+
+export default EducationInput;
