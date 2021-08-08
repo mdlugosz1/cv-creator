1d6ffceca38a2a70ec315d64a2a06dcbdb21ea52
Michał Długosz <mdugosz@gmail.com>
2021-08-08 18:17:15 +0200

    Refactor into functional component

--- src/components/Utilities/Form.js
+++ src/components/Utilities/Form.js
@@ -2,101 +2,91 @@ import React from 'react';
 import Button from './Button';
 import '../../styles/Form.css';
 
-export default class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.addInput = this.addInput.bind(this);
-        this.removeInput = this.removeInput.bind(this);
+const Form = (props) => {
+  const addInput = (e) => {
+    e.preventDefault();
+    props.createInput();
+  };
+
+  const removeInput = (e) => {
+    e.preventDefault();
+    props.deleteInput(e);
+  };
+
+  const setInputType = (input) => {
+    let inputType;
+
+    if (input.tag) {
+      inputType = (
+        <input.tag
+          name={input.name}
+          key={input.id}
+          placeholder={input.placeholder}
+          onChange={props.handleChange}
+          data-parent-id={input.parentID}
+          onKeyPress={(e) => {
+            e.key === 'Enter' && e.preventDefault();
+          }}
+        />
+      );
+    } else {
+      inputType = (
+        <input
+          name={input.name}
+          key={input.id}
+          type={input.type}
+          placeholder={input.placeholder}
+          onChange={props.handleChange}
+          data-parent-id={input.parentID}
+          onKeyPress={(e) => {
+            e.key === 'Enter' && e.preventDefault();
+          }}
+        />
+      );
     }
 
-    addInput(e) {
-        e.preventDefault();
-        this.props.createInput();
-    }
+    return inputType;
+  };
 
-    removeInput(e) {
-        e.preventDefault();
-        this.props.deleteInput(e);
-    }
-
-    setInputType(input) {
-        let inputType;
-
-        if (input.tag) {
-            inputType = (
-                <input.tag
-                    name={input.name}
-                    key={input.id}
-                    placeholder={input.placeholder}
-                    onChange={this.props.handleChange}
-                    data-parent-id={input.parentID}
-                    onKeyPress={(e) => {
-                        e.key === 'Enter' && e.preventDefault();
-                    }}
-                />
-            );
+  return (
+    <form>
+      {props.inputs.map((element) => {
+        if (Object.keys(element).includes('inputs')) {
+          return (
+            <div data-main-id={element.mainID} key={element.mainID}>
+              {element.inputs.map((input) => {
+                return setInputType(input);
+              })}
+              <Button name={'Remove'} click={removeInput} />
+            </div>
+          );
         } else {
-            inputType = (
-                <input
-                    name={input.name}
-                    key={input.id}
-                    type={input.type}
-                    placeholder={input.placeholder}
-                    onChange={this.props.handleChange}
-                    data-parent-id={input.parentID}
-                    onKeyPress={(e) => {
-                        e.key === 'Enter' && e.preventDefault();
-                    }}
-                />
-            );
+          return (
+            <div key={element.id}>
+              <input
+                name={element.name}
+                type={element.type}
+                data-id={element.id}
+                onChange={props.handleChange}
+                placeholder={element.placeholder}
+                onKeyPress={(e) => {
+                  e.key === 'Enter' && e.preventDefault();
+                }}
+              />
+              {element.button && (
+                <Button name={element.button} click={removeInput} data={element.id} />
+              )}
+            </div>
+          );
         }
+      })}
+      {props.button && (
+        <div>
+          <Button name={props.button} click={addInput} class="add-input" />
+        </div>
+      )}
+    </form>
+  );
+};
 
-        return inputType;
-    }
-
-    render() {
-        return (
-            <form>
-                {this.props.inputs.map((element) => {
-                    if (Object.keys(element).includes('inputs')) {
-                        return (
-                            <div data-main-id={element.mainID} key={element.mainID}>
-                                {element.inputs.map((input) => {
-                                    return this.setInputType(input);
-                                })}
-                                <Button name={'Remove'} click={this.removeInput} />
-                            </div>
-                        );
-                    } else {
-                        return (
-                            <div key={element.id}>
-                                <input
-                                    name={element.name}
-                                    type={element.type}
-                                    data-id={element.id}
-                                    onChange={this.props.handleChange}
-                                    placeholder={element.placeholder}
-                                    onKeyPress={(e) => {
-                                        e.key === 'Enter' && e.preventDefault();
-                                    }}
-                                />
-                                {element.button && (
-                                    <Button
-                                        name={element.button}
-                                        click={this.removeInput}
-                                        data={element.id}
-                                    />
-                                )}
-                            </div>
-                        );
-                    }
-                })}
-                {this.props.button && (
-                    <div>
-                        <Button name={this.props.button} click={this.addInput} class="add-input" />
-                    </div>
-                )}
-            </form>
-        );
-    }
-}
+export default Form;
