d05029cb08a19016a57951c525bd578e28dbd8fa
Michał Długosz <mdugosz@gmail.com>
2021-08-08 23:19:48 +0200

    Refactor into functional component, add hooks

--- src/components/WorkInput.js
+++ src/components/WorkInput.js
@@ -1,67 +1,117 @@
-import React from 'react';
-import ProjectsInput from './ProjectsInput';
+import React, { useState } from 'react';
 import uniqid from 'uniqid';
 import Form from './Utilities/Form';
 
-export default class WorkInput extends ProjectsInput {
-    constructor(props) {
-        super(props);
-        this.state = {
-            work: [this.inputTemplate()],
-            button: 'Add',
-        };
-    }
+const WorkInput = (props) => {
+  const [workInputs, setWorkInputs] = useState({
+    work: [
+      {
+        mainID: uniqid(),
+        inputs: [
+          {
+            id: uniqid(),
+            type: 'text',
+            name: 'position',
+            placeholder: 'Position',
+          },
+          {
+            id: uniqid(),
+            type: 'text',
+            name: 'company',
+            placeholder: 'Company',
+          },
+          {
+            id: uniqid(),
+            type: 'text',
+            name: 'startDate',
+            placeholder: 'Start date MM/YYYY',
+          },
+          {
+            id: uniqid(),
+            name: 'endDate',
+            type: 'text',
+            placeholder: 'End date MM/YYYY',
+          },
+          {
+            id: uniqid(),
+            name: 'responsibilities',
+            tag: 'textarea',
+            placeholder: 'Responsibilities/goals reached',
+          },
+        ],
+      },
+    ],
+    button: 'Add',
+  });
 
-    inputTemplate() {
-        return {
-            mainID: uniqid(),
-            inputs: [
-                {
-                    id: uniqid(),
-                    type: 'text',
-                    name: 'position',
-                    placeholder: 'Position',
-                },
-                {
-                    id: uniqid(),
-                    type: 'text',
-                    name: 'company',
-                    placeholder: 'Company',
-                },
-                {
-                    id: uniqid(),
-                    type: 'text',
-                    name: 'startDate',
-                    placeholder: 'Start date MM/YYYY',
-                },
-                {
-                    id: uniqid(),
-                    name: 'endDate',
-                    type: 'text',
-                    placeholder: 'End date MM/YYYY',
-                },
-                {
-                    id: uniqid(),
-                    name: 'responsibilities',
-                    tag: 'textarea',
-                    placeholder: 'Responsibilities/goals reached',
-                },
-            ],
-        };
-    }
+  const inputTemplate = () => {
+    return {
+      mainID: uniqid(),
+      inputs: [
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'position',
+          placeholder: 'Position',
+        },
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'company',
+          placeholder: 'Company',
+        },
+        {
+          id: uniqid(),
+          type: 'text',
+          name: 'startDate',
+          placeholder: 'Start date MM/YYYY',
+        },
+        {
+          id: uniqid(),
+          name: 'endDate',
+          type: 'text',
+          placeholder: 'End date MM/YYYY',
+        },
+        {
+          id: uniqid(),
+          name: 'responsibilities',
+          tag: 'textarea',
+          placeholder: 'Responsibilities/goals reached',
+        },
+      ],
+    };
+  };
 
-    render() {
-        const { work, button } = this.state;
-        return (
-            <div>
-                <Form
-                    inputs={work}
-                    button={button}
-                    handleChange={this.handleChange}
-                    deleteInput={this.removeInputBlock}
-                    createInput={this.createNewInputs}
-                />
-            </div>
-        );
-    }
-}
+  const handleChange = (e) => {
+    const key = props.getMainKey(workInputs);
+    props.onInputChange({
+      [key]: {
+        [e.target.name]: e.target.value,
+        id: e.target.closest('div').dataset.mainId,
+      },
+    });
+  };
+
+  const removeInputBlock = (e) => {
+    props.removeInput(workInputs, props, setWorkInputs, e);
+  };
+
+  const createNewInputs = () => {
+    const newInput = inputTemplate();
+    props.addInput(newInput, props, workInputs, setWorkInputs);
+  };
+
+  return (
+    <div>
+      <Form
+        inputs={workInputs.work}
+        button={workInputs.button}
+        handleChange={handleChange}
+        deleteInput={removeInputBlock}
+        createInput={createNewInputs}
+      />
+    </div>
+  );
+};
+
+export default WorkInput;
