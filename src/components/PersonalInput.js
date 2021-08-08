be3eb3d1cfb8a2a3986d025c07555337453a5c9a
Michał Długosz <mdugosz@gmail.com>
2021-08-08 18:32:40 +0200

    Refactor into functional component, add hooks

--- src/components/PersonalInput.js
+++ src/components/PersonalInput.js
@@ -1,53 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './Utilities/Form';
 import uniqid from 'uniqid';
 
-export default class PersonalInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      inputs: [
-        { type: 'text', name: 'name', placeholder: 'First Name', id: uniqid() },
-        {
-          type: 'text',
-          name: 'lastname',
-          placeholder: 'Last Name',
-          id: uniqid(),
-        },
-        { type: 'text', name: 'position', placeholder: 'Title', id: uniqid() },
-        { type: 'text', name: 'mail', placeholder: 'E-mail', id: uniqid() },
-        {
-          type: 'text',
-          name: 'phone',
-          placeholder: 'Phone Number',
-          id: uniqid(),
-        },
-        {
-          type: 'text',
-          name: 'webpage',
-          placeholder: 'Webpage URL',
-          id: uniqid(),
-        },
-        { type: 'text', name: 'city', placeholder: 'City', id: uniqid() },
-      ],
-    };
-  }
+const PersonalInput = (props) => {
+  const [inputs] = useState([
+    { type: 'text', name: 'name', placeholder: 'First Name', id: uniqid() },
+    {
+      type: 'text',
+      name: 'lastname',
+      placeholder: 'Last Name',
+      id: uniqid(),
+    },
+    { type: 'text', name: 'position', placeholder: 'Title', id: uniqid() },
+    { type: 'text', name: 'mail', placeholder: 'E-mail', id: uniqid() },
+    {
+      type: 'text',
+      name: 'phone',
+      placeholder: 'Phone Number',
+      id: uniqid(),
+    },
+    {
+      type: 'text',
+      name: 'webpage',
+      placeholder: 'Webpage URL',
+      id: uniqid(),
+    },
+    { type: 'text', name: 'city', placeholder: 'City', id: uniqid() },
+  ]);
 
-  handleChange(e) {
+  const handleChange = (e) => {
     const target = e.target;
     const value = target.value;
     const key = target.name;
 
-    this.props.onInputChange({ personal: { [key]: value } });
-  }
+    props.onInputChange({ personal: { [key]: value } });
+  };
 
-  render() {
-    const { inputs } = this.state;
-    return (
-      <div>
-        <Form inputs={inputs} handleChange={this.handleChange} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Form inputs={inputs} handleChange={handleChange} />
+    </div>
+  );
+};
+
+export default PersonalInput;
